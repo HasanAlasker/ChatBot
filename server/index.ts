df4275b7896481fc75b8b2d0b1b0b870fa780b1f
Hasan Alasker <hasanalasker2004@gmail.com>
2026-07-30 15:51:28 +0300

    set up and integrate

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,21 @@
 import express, { json, type Request, type Response } from "express";
 import dotenv from "dotenv";
+import cors from 'cors'
 
 dotenv.config();
 const app = express();
 
 const port = process.env.PORT ?? "4000";
 
+app.use(cors({origin: "*"}))
+
 app.use(json());
 app.get("/", async (req: Request, res: Response) => {
   res.send("Hello Hasan Alasker");
+});
+
+app.get("/api/hello", async (req: Request, res: Response) => {
+  res.json({ message: "Hello world" }).status(200);
 });
 
 app.listen(port, () => {
