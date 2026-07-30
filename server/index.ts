9d6ec049811a48bc608b77d42c867eb4cfaec8af
Hasan Alasker <hasanalasker2004@gmail.com>
2026-07-30 14:56:10 +0300

    create client

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import express, { json, type Request, type Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT ?? "4000";
+
+app.use(json());
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Hello Hasan Alasker");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port} 🌍`);
+  console.log(`Accessible at http://localhost:${port} 🖥️`);
+});
